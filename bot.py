52787400380e310d8b6791d8dfc7d898785d2cdc
Nick-BhY <64404680+Nick-BhY@users.noreply.github.com>
2025-06-09 19:08:46 +0500

    bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,12 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.types import Message
 
-from handlers.common import router as common_router
-from keyboards.inline import some_keyboard
-
-TOKEN = "8190912145:AAF0JZIrSyblCJXtqdJVYEDA_sT0jJI0RIM"
-bot = Bot(token=TOKEN)
+bot = Bot(token="8190912145:AAF0JZIrSyblCJXtqdJVYEDA_sT0jJI0RIM")
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await message.reply("Привет! Меня зовут Валерия Марченко. Я хореограф.")
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.reply(message.text)
+async def start(message: Message):
+    await message.answer("Привет! Меня зовут Валерия Марченко. Я сделаю тебя красивой")
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp)
