5c3fb4237e0ac6d00caed930449448ffb6b40898
Nick-BhY <64404680+Nick-BhY@users.noreply.github.com>
2025-06-09 17:44:26 +0500

    Teleg-bot

---
+++ bot.py
@@ -0,0 +1,16 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+TOKEN = "8190912145:AAF0JZIrSyblCJXtqdJVYEDA_sT0jJI0RIM"
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.reply("Привет! Меня зовут Валерия Марченко. Я хореограф.")
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.reply(message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
